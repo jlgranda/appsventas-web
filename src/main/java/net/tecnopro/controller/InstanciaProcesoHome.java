f901f27222003f69236bc9cd5dee6620673619a6
jlgranda <jlgranda81@gmail.com>
2016-03-22 13:48:46 -0500

    Se agrega soporte para formulario de respuesta en vista de instancia de proceso

--- src/main/java/net/tecnopro/controller/InstanciaProcesoHome.java
+++ src/main/java/net/tecnopro/controller/InstanciaProcesoHome.java
@@ -16,24 +16,37 @@
  */
 package net.tecnopro.controller;
 
+import com.google.common.base.Strings;
 import com.jlgranda.fede.SettingNames;
+import com.jlgranda.fede.ejb.SubjectService;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import net.tecnopro.document.ejb.DocumentoService;
 import net.tecnopro.document.ejb.InstanciaProcesoService;
 import net.tecnopro.document.ejb.TareaService;
+import net.tecnopro.document.model.Documento;
+import net.tecnopro.document.model.EstadoTipo;
 import net.tecnopro.document.model.InstanciaProceso;
 import net.tecnopro.document.model.Tarea;
 import org.jlgranda.fede.cdi.LoggedIn;
 import org.jlgranda.fede.controller.FedeController;
 import org.jlgranda.fede.controller.SettingHome;
+import org.jlgranda.fede.ui.util.SubjectConverter;
 import org.jpapi.model.Group;
 import org.jpapi.model.profile.Subject;
 import org.jpapi.util.Dates;
+import org.jpapi.util.I18nUtil;
+import org.jpapi.util.Lists;
 import org.primefaces.event.SelectEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,8 +77,24 @@ public class InstanciaProcesoHome extends FedeController implements Serializable
     @EJB
     private TareaService tareaService;
 
-    private Tarea ultimaTarea;
+    private Tarea tarea;
+
     private List<Tarea> tareas;
+
+    private Subject solicitante;
+
+    private Subject destinatario;
+
+    @EJB
+    private SubjectService subjectService;
+
+    @EJB
+    private DocumentoService documentoService;
+
+    private List<Documento> documentosRemovidos = new ArrayList<>();
+    
+    //UI variables
+    private String activeIndex;
 
     @PostConstruct
     public void init() {
@@ -80,7 +109,9 @@ public class InstanciaProcesoHome extends FedeController implements Serializable
         setStart(Dates.addDays(getEnd(), -1 * amount));
 
         setOutcome("procesos");
-        setInstanciaProceso(instanciaProcesoService.createInstance());
+
+        setTarea(tareaService.createInstance()); //Siempre listo para recibir la respuesta del proceso
+
         //TODO Establecer temporalmente la organización por defecto
         //getOrganizationHome().setOrganization(organizationService.find(1L));
     }
@@ -94,7 +125,7 @@ public class InstanciaProcesoHome extends FedeController implements Serializable
     }
 
     public InstanciaProceso getInstanciaProceso() {
-        if (this.instanciaProcesoId != null && !this.instanciaProceso.isPersistent()) {
+        if (this.instanciaProcesoId != null && this.instanciaProceso == null) {
             this.instanciaProceso = instanciaProcesoService.find(instanciaProcesoId);
         }
         return instanciaProceso;
@@ -112,28 +143,151 @@ public class InstanciaProcesoHome extends FedeController implements Serializable
         this.subject = subject;
     }
 
-    public Tarea getUltimaTarea() {
-        if (instanciaProcesoId != null && this.instanciaProceso.isPersistent()) {
-            List<Tarea> tareasUltima = tareaService.findByNamedQuery("Tarea.findLastByInstanciaProceso", getInstanciaProceso());
-            ultimaTarea = tareasUltima.isEmpty() ? new Tarea() : (Tarea) tareasUltima.get(0);
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+    public Subject getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Subject solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public Subject getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(Subject destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public void send() {
+        try {
+
+            //Actualizar tarea pendiente
+            for (Tarea t : getInstanciaProceso().getTareas()) {
+                if (EstadoTipo.ESPERA.equals(t.getEstadoTipo())) {
+                    t.setEstadoTipo(EstadoTipo.RESUELTO);
+                }
+            }
+
+            //Preparar tarea para envio
+            tarea.setAuthor(subject);
+            tarea.setOwner(getDestinatario());
+            tarea.setDepartamento("Temporal");
+            tarea.setEstadoTipo(EstadoTipo.ESPERA);
+            procesarDocumentos(tarea);
+            eliminarDocumentos();
+
+            getInstanciaProceso().addTarea(tarea);
+
+            instanciaProcesoService.save(getInstanciaProceso().getId(), getInstanciaProceso());
+
+            //Encerar tarea para recoger nueva respuesta
+            setTarea(tareaService.createInstance());
+            setDestinatario(null);
+            setActiveIndex(null);
+
+            this.addDefaultSuccessMessage();
+        } catch (Exception e) {
+            addErrorMessage(e, I18nUtil.getMessages("error.persistence"));
         }
-        return ultimaTarea;
     }
 
-    public void setUltimaTarea(Tarea ultimaTarea) {
-        this.ultimaTarea = ultimaTarea;
-    }
-
-    public List<Tarea> getTareas() {
-        if (instanciaProcesoId != null && this.instanciaProceso.isPersistent()) {
-            List<Tarea> tareasInstanciaProceso = tareaService.findByNamedQuery("Tarea.findLastsByInstanciaProceso", getInstanciaProceso());
-            tareas = tareasInstanciaProceso;
+    public void procesarDocumentos(Tarea t) {
+        for (Documento doc : t.getDocumentos()) {
+            if (!doc.isPersistent()) {
+                documentoService.save(doc);
+            } else {
+                documentoService.save(doc.getId(), doc);
+            }
+            generaDocumento(new File(doc.getRuta()), doc.getContents());
         }
-        return tareas;
     }
 
-    public void setTareas(List<Tarea> tareas) {
-        this.tareas = tareas;
+    public void eliminarDocumentos() {
+        for (Documento doc : documentosRemovidos) {
+            if (doc.isPersistent()) {
+                documentoService.remove(doc.getId(), doc);
+            }
+            documentosRemovidos.remove(doc);
+        }
+    }
+
+    public void generaDocumento(File file, byte[] bytes) {
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            try (BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                bos.write(bytes);
+                bos.flush();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            addErrorMessage(ex, I18nUtil.getMessages("common.error.uploadfail"));
+        }
+    }
+
+    public List<Subject> completeSubjects(final String query) {
+        List<Subject> result = new ArrayList<>();
+        if (!"".equals(query.trim())) {
+            Subject subjectBuscar = new Subject();
+            subjectBuscar.setUsername(query);
+            subjectBuscar.setFirstname(query);
+            subjectBuscar.setSurname(query);
+            result = subjectService.buscarPorCriterio(subjectBuscar);
+        }
+        SubjectConverter.setSubjects(result);
+        return result;
+    }
+
+    
+    public void setActiveIndex (String activeIndex){
+        this.activeIndex = activeIndex;
+    }
+    
+    /**
+     * Calcula los indices de los tabs a mostrar expandidos, en función del
+     * estado de las tareas de la instancia de proceso
+     *
+     * @return lista de indices separadas por coma
+     */
+    public String getActiveIndex() {
+        if (Strings.isNullOrEmpty(activeIndex)) {
+            if (getInstanciaProceso() == null) {
+                activeIndex = "";
+            } else {
+                List<Integer> indexs = new ArrayList<>();
+                for (int i = 0; i < getInstanciaProceso().getTareas().size(); i++) {
+                    if (EstadoTipo.ESPERA.equals(getInstanciaProceso().getTareas().get(i).getEstadoTipo())) {
+                        indexs.add(i);
+                    }
+                }
+                activeIndex = Lists.toString(indexs).trim();
+            }
+        }
+
+        return activeIndex;
+    }
+
+    public boolean calculeShowResponseForm() {
+        boolean showResponseForm = false;
+        if (getInstanciaProceso() != null) {
+            for (Tarea t : getInstanciaProceso().getTareas()) {
+                //Sólo hay una tarea a la espera y corresponde al usuario actual
+                if (EstadoTipo.ESPERA.equals(t.getEstadoTipo()) && subject.equals(t.getOwner())) {
+                    showResponseForm = true;
+                    break; //finalizar bucle, ya se econtro.
+                }
+            }
+        }
+        return showResponseForm;
     }
 
     @Override
