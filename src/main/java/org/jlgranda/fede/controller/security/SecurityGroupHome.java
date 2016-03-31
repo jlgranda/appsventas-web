5c51ac1a8c2d909a539c7cf8a60ff877d3dbcbb1
Jorge <jorge.malla@unl.edu.ec>
2016-03-31 00:13:21 -0500

    Funcionalidad Adinistrar Grupos

---
+++ src/main/java/org/jlgranda/fede/controller/security/SecurityGroupHome.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2013 cesar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jlgranda.fede.controller.security;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.TransactionAttribute;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import org.picketlink.Identity;
+import org.picketlink.idm.api.Group;
+import org.picketlink.idm.api.IdentitySession;
+import org.picketlink.idm.common.exception.IdentityException;
+import org.picketlink.idm.impl.api.model.SimpleGroup;
+
+/**
+ *
+ * @author cesar
+ */
+@Named
+@ViewScoped
+public class SecurityGroupHome implements Serializable {
+
+    private static final long serialVersionUID = 7632987414391869389L;
+
+    @Inject
+    private EntityManager entityManager;
+    @Inject
+    private Identity identity;
+    @Inject
+    private IdentitySession security;
+    private Group instance;
+    @Inject
+    private SecurityGroupService securityGroupService;
+    private String groupKey;
+    private String groupName;
+
+    @PostConstruct
+    public void init() {
+        securityGroupService.setSecurity(security);
+    }
+
+    public String getGroupKey() {
+        return groupKey;
+    }
+
+    public void setGroupKey(String groupKey) {
+        this.groupKey = groupKey;
+    }
+
+    public String getGroupName() {
+        if (isManaged()) {
+            setGroupName(getInstance().getName());
+        }
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void setInstance(Group instance) {
+        this.instance = instance;
+    }
+
+    public Group getInstance() {
+        if (instance == null) {
+            initInstance();
+        }
+        return instance;
+    }
+
+    @TransactionAttribute
+    public String saveGroup() {
+        if (isManaged()) {
+            //TODO implementar actualización de nombre de grupo, evaluar si es necesario
+        } else {
+            try {
+                security.getPersistenceManager().createGroup(getGroupName(), "GROUP");
+            } catch (IdentityException ex) {
+                Logger.getLogger(SecurityGroupHome.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return "/pages/admin/security/list?faces-redirect=true";
+    }
+
+    protected Group createInstance() {
+        Group u = new SimpleGroup("New Group", "GROUP");
+        return u;
+    }
+
+    @TransactionAttribute
+    public void load() {
+        if (isIdDefined()) {
+            wire();
+        }
+    }
+
+    @TransactionAttribute
+    public void wire() {
+        getInstance();
+    }
+
+    public boolean isPersistent() {
+        return getInstance().getKey() != null;
+    }
+
+    public boolean isIdDefined() {
+        return getGroupKey() != null && !"".equals(getGroupKey());
+    }
+
+    public Group find() throws IdentityException {
+        if (securityGroupService.getSecurity() != null) {
+            Group result = securityGroupService.findByKey(getGroupKey());
+            if (result == null) {
+                result = handleNotFound();
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    public void clearInstance() {
+        setInstance(null);
+        setGroupKey(null);
+    }
+
+    protected void initInstance() {
+        if (isIdDefined()) {
+            if (true /*!isTransactionMarkedRollback()*/) {
+                try {
+                    //we cache the instance so that it does not "disappear"
+                    //after remove() is called on the instance
+                    //is this really a Good Idea??
+                    setInstance(find());
+                } catch (IdentityException ex) {
+                    Logger.getLogger(SecurityGroupHome.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            setInstance(createInstance());
+        }
+    }
+
+    private Group handleNotFound() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean isManaged() {
+        return getInstance() != null
+                && getGroupKey() != null;
+    }
+}
---
+++ src/main/java/org/jlgranda/fede/controller/security/SecurityGroupService.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2013 cesar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jlgranda.fede.controller.security;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import org.jpapi.util.QuerySortOrder;
+import org.picketlink.idm.api.Group;
+import org.picketlink.idm.api.IdentitySession;
+import org.picketlink.idm.common.exception.IdentityException;
+import org.picketlink.idm.impl.api.IdentitySearchCriteriaImpl;
+import org.picketlink.idm.api.User;
+import org.picketlink.idm.api.SortOrder;
+import org.picketlink.idm.api.UnsupportedCriterium;
+
+/**
+ *
+ * @author cesar
+ */
+public class SecurityGroupService implements Serializable {
+
+    private static final long serialVersionUID = -8856264241192917839L;
+    private EntityManager entityManager;
+    private IdentitySession security;
+
+    public SecurityGroupService() {
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public IdentitySession getSecurity() {
+        return security;
+    }
+
+    public void setSecurity(IdentitySession security) {
+        this.security = security;
+    }
+
+    //metodo count
+    public long count() {
+        try {
+            return (long) security.getPersistenceManager().getGroupTypeCount("GROUP");
+        } catch (IdentityException ex) {
+            Logger.getLogger(SecurityGroupService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    public Group getGroupById(final Long id) throws IdentityException {
+        Group g = security.getPersistenceManager().findGroupByKey(String.valueOf(id));
+
+        return g;
+
+    }
+
+    public Group findByName(final String name) throws IdentityException {
+
+        return (Group) security.getPersistenceManager().findGroup(name, "GROUP");
+    }
+
+    public Group findByKey(final String key) throws IdentityException {
+        return security.getPersistenceManager().findGroupByKey(key);
+    }
+
+    public List<Group> find(int first, int end, String sortField, QuerySortOrder order, Map<String, Object> _filters) throws UnsupportedCriterium, IdentityException {
+
+        IdentitySearchCriteriaImpl identitySearchCriteria = new IdentitySearchCriteriaImpl();
+        identitySearchCriteria.sort(SortOrder.ASCENDING);
+        if (QuerySortOrder.DESC.equals(order)) {
+            identitySearchCriteria.sort(SortOrder.DESCENDING);
+        }
+        identitySearchCriteria.sortAttributeName(sortField);
+        identitySearchCriteria.setPaged(true);
+        identitySearchCriteria.page(first, end);
+        String[] values = new String[1];
+        for (Map.Entry entry : _filters.entrySet()) {
+            values[1] = (String) entry.getValue();
+            identitySearchCriteria.attributeValuesFilter((String) entry.getKey(), values);
+        }
+
+        List<Group> tem = new ArrayList<Group>(security.getPersistenceManager().findGroup("GROUP", identitySearchCriteria));
+        return tem;
+
+    }
+
+    public void removeGroup(Group g) throws IdentityException {
+        security.getPersistenceManager().removeGroup(g, true);
+    }
+
+    public void associate(Group g, User u) throws IdentityException {
+        security.getRelationshipManager().associateUser(g, u);
+    }
+
+    public void disassociate(Group g, User u) throws IdentityException {
+        Collection<User> listUser = new ArrayList<User>();
+        listUser.add(u);
+        security.getRelationshipManager().disassociateUsers(g, listUser);
+    }
+
+    public User findUser(String usr) throws IdentityException {
+        return security.getPersistenceManager().findUser(usr);
+    }
+
+    Collection<Group> find(User user) throws IdentityException {
+        return security.getRelationshipManager().findAssociatedGroups(user);
+    }
+
+    boolean isAssociated(Group group, User user) throws IdentityException {
+        return security.getRelationshipManager().isAssociated(group, user);
+    }
+
+    boolean isAssociatedUser(Group group) throws IdentityException {
+        boolean b = security.getRelationshipManager().findAssociatedUsers(group, true).isEmpty();
+        return b;
+    }
+}
