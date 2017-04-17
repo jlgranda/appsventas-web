5bc9cc9437dde3fbbd38afce510fff0699736211
José Luis Granda <jlgranda81@gmail.com>
2017-04-17 15:08:58 -0500

    Implementación de impresión de factura

--- src/main/java/org/jlgranda/fede/servlet/FedeServlet.java
+++ src/main/java/org/jlgranda/fede/servlet/FedeServlet.java
@@ -23,6 +23,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,7 +33,7 @@ import org.jpapi.util.Strings;
  *
  * @author Jorge
  */
-//@WebServlet(name = "fedeServlet", urlPatterns = {"/fedeServlet/*"})
+@WebServlet(name = "fedeServlet", urlPatterns = {"/fedeServlet/*"})
 public class FedeServlet extends HttpServlet {
 
 //    @Inject
