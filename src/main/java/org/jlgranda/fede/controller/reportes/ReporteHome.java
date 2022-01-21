ca035c86d373043428e5b5fd1f859f0e151357e5
kellypaulinc <kelly.narvaez1@gmail.com>
2022-01-21 16:15:26 -0500

    Reportes

---
+++ src/main/java/org/jlgranda/fede/controller/reportes/ReporteHome.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2022 usuario
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jlgranda.fede.controller.reportes;
+
+import com.jlgranda.fede.ejb.reportes.ReporteService;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.jlgranda.fede.controller.FedeController;
+import org.jlgranda.fede.controller.OrganizationData;
+import org.jlgranda.fede.model.accounting.Record;
+import org.jlgranda.fede.model.reportes.Reporte;
+import org.jpapi.model.Group;
+import org.jpapi.model.profile.Subject;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.LazyDataModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author usuario
+ */
+@Named
+@ViewScoped
+public class ReporteHome extends FedeController implements Serializable {
+
+    Logger logger = LoggerFactory.getLogger(ReporteHome.class);
+
+    @Inject
+    private Subject subject;
+    @Inject
+    private OrganizationData organizationData;
+    
+    @EJB
+    private ReporteService reporteService;
+    
+    private LazyDataModel reportes;
+    
+    private Reporte reporte;
+    
+    @PostConstruct
+    private void init(){
+        
+    }
+
+    public LazyDataModel getReportes() {
+        return reportes;
+    }
+
+    public void setReportes(LazyDataModel reportes) {
+        this.reportes = reportes;
+    }
+
+    public Reporte getReporte() {
+        return reporte;
+    }
+
+    public void setReporte(Reporte reporte) {
+        this.reporte = reporte;
+    }
+    
+    
+    
+    @Override
+    public void handleReturn(SelectEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Record aplicarReglaNegocio(String nombreRegla, Object fuenteDatos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Group getDefaultGroup() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void initializeDateInterval() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Group> getGroups() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
